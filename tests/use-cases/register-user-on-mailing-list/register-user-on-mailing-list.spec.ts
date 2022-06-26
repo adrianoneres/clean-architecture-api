d0834d4cabc9a6b72d1154016bd04a1286ce04fa
Adriano Souza <ah.driano@gmail.com>
2022-06-26 18:43:30 -0400

    fix: typo on Register

--- tests/use-cases/register-user-on-mailing-list/register-user-on-mailing-list.spec.ts
+++ tests/use-cases/register-user-on-mailing-list/register-user-on-mailing-list.spec.ts
@@ -1,6 +1,6 @@
 import { UserData } from '../../../src/use-cases/register-user-on-mailing-list/user-data';
 
-describe('Regiser user on mailing list use case', () => {
+describe('Register user on mailing list use case', () => {
   test('should add user with complete data to mailing list', async () => {
     const userData: UserData = { name: '', email: '' };
     console.log(userData);
